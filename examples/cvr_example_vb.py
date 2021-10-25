210384b7b12bed8b36c25361db66ad8393b21dfb
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-10-25 11:19:38 +0100

    Tidied up options a bit in VB example

--- examples/cvr_example_vb.py
+++ examples/cvr_example_vb.py
@@ -12,32 +12,25 @@ model = "cvr_petco2"
 outdir = "cvr_example_vb_out"
 
 import numpy as np
-phys_data = np.loadtxt("phys_data.txt")
+pco2 = np.loadtxt("pco2.txt")
 
 options = {
-    "phys_data" : phys_data[:, 1],
+    "phys_data" : pco2,
+    "tr" : 0.8,
     "infer_sig0" : True,
     "infer_delay" : True,
     "save_mean" : True,
     "save_noise" : True,
-    "save_param_history" : True,
-    "tr" : 0.8,
-    #"save_free_energy_history" : True,
+    "save_param_history" : False,
     "save_runtime" : True,
     "save_free_energy" : True,
     "save_model_fit" : True,
-    "save_log" : True,
     "save_input_data" : True,
-    "save_var" : True,
+    "save_var" : False,
+    "save_log" : True,
     "log_stream" : sys.stdout,
     "max_iterations" : 50,
-#    "param_overrides" : {
-#        "cvr" : {
-#            "dist" : "LogNormal",
-#        }
-#    }
 }
 
-runtime, avb = run("filtered_func_data.nii.gz", model, "cvr_example_vb_out", mask="small.nii.gz", **options)
+runtime, avb = run("filtered_func_data.nii.gz", model, "cvr_example_vb_out", mask="mask.nii.gz", **options)
 #runtime, avb = run("raw_bold.nii.gz", model, "raw_bold_vb_out", mask="mask.nii.gz", **options)
-
