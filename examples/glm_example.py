7a2727dbb9d4c3d14442161f7d178203d2c66ed9
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-10-25 11:19:58 +0100

    Tidy up examples and add logging to GLM example

--- examples/glm_example.py
+++ examples/glm_example.py
@@ -2,29 +2,26 @@ import sys
 import os
 
 from vaby.data import DataModel
+from vaby.utils import setup_logging
 from vaby_models_cvr import CvrPetCo2Model
 
 options = {
-    "phys_data" : "phys_data.txt",
-    "infer_sig0" : True,
-    "infer_delay" : True,
+    "phys_data" : "pco2.txt",
+    "tr" : 0.8,
     "save_mean" : True,
-    "save_noise" : True,
-    "save_param_history" : True,
     #"save_free_energy_history" : True,
     "save_runtime" : True,
-    "save_free_energy" : True,
     "save_model_fit" : True,
-    "save_log" : True,
     "save_input_data" : True,
-    "save_var" : True,
+    "save_log" : True,
     "log_stream" : sys.stdout,
     "max_iterations" : 50,
 }
 
 OUTDIR="glm_example_out"
-os.makedirs(OUTDIR)
+os.makedirs(OUTDIR, exist_ok=True)
 
+setup_logging(OUTDIR, **options)
 data_model = DataModel("filtered_func_data.nii.gz", mask="mask.nii.gz")
 model = CvrPetCo2Model(data_model, **options)
 cvr, delay, sig0, modelfit = model.fit_glm(delay_min=-10, delay_max=10, delay_step=2)
